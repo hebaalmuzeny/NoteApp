fda2cf96eea577a7064c1fea68f4c53bfc0e2149
Daliasalah <saliasary46@gmail.com>
2019-12-27 11:02:26 +0200

    edit Signup

--- Final/app/src/main/java/com/example/afinal/Signup.java
+++ Final/app/src/main/java/com/example/afinal/Signup.java
@@ -8,16 +8,41 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Signup extends AppCompatActivity {
 
-    EditText email, pass;
-    Button signup;
+    EditText emailId, password;
+    Button signUp;
     FirebaseAuth mFirebaseAuth;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        emailId = findViewById(R.id.email);
+        password = findViewById(R.id.pass);
+        signUp = findViewById(R.id.signup);
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+             String email1 = emailId.getText().toString();
+             String psw = password.getText().toString();
+             if(email1.isEmpty()){
+                 emailId.setError("please enter email id");
+                 emailId.requestFocus();
+             }
+             else if(psw.isEmpty()){
+                 password.setError("please enter password");
+                 password.requestFocus();
+             }
+            }
+        }){
+
+        }
     }
+
 
     public void btn_back(View view) {
         Intent intent1 = new Intent(Signup.this, MainActivity.class);
