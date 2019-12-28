0924035a9125686ef93e526e3bd627a18359456f
Daliasalah <saliasary46@gmail.com>
2019-12-28 20:56:44 +0200

    Auth

--- Final/app/src/main/java/com/example/afinal/Signup.java
+++ Final/app/src/main/java/com/example/afinal/Signup.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,6 +13,7 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
@@ -32,51 +34,52 @@ public class Signup extends AppCompatActivity {
     Button signUp;
     FirebaseAuth mFirebaseAuth;
 
-    protected void onCreate(Bundle savedInstanceState) {
+    public void Oncreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
-
         mFirebaseAuth = FirebaseAuth.getInstance();
-        FirebaseUser user = mFirebaseAuth.getCurrentUser();
-
-        if (user!=null)
-        {
-            Intent myintent = new Intent(Signup.this , Home.class);
-            startActivity(myintent);
-        }
-
 
         emailId = findViewById(R.id.email);
         password = findViewById(R.id.pass);
         signUp = findViewById(R.id.signup);
-        //signUp.setOnClickListener(new View.OnClickListener(v-> {
-         //   doSignup(emailId.getText().toString() , password.getText().toString());
-        });
-        private void doSignup(String emailId, String password){
-            mFirebaseAuth.createUserWithEmailAndPassword(emailId,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                @Override
-                public void onComplete(@NonNull Task<AuthResult> task) {
-                    if (task.isSuccessful()) {
-                        FirebaseUser user = mFirebaseAuth.getCurrentUser();
-                        String emailF = user.getEmail();
-                        String uid = user.getUid();
-                        Map<String, Object> data = new HashMap<>();
-                        data.put("uid", uid);
-                        data.put("email", emailF);
-                        data.put("createdAt", new Date().getTime());
 
-                        // FirebaseDatabase.getInstance().getReference().child("User").child(uid).setValue(data).addOnFailureListener(new OnFailureListener() {
-
-                        //  })
-
-
-                    }
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailId.getText().toString().trim();
+                String pass = password.getText().toString().trim();
+                if(TextUtils.isEmpty(email)){
+                    Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                    return;
                 }
+                if(TextUtils.isEmpty(pass)){
+                    Toast.makeText(getApplicationContext(), "Enter Password!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (password.length() < 6) {
+                    Toast.makeText(getApplicationContext(), "Password too short, enter minimum 6 characters!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                mFirebaseAuth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(Signup.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Toast.makeText(Signup.this, "created account is onComplete" + task.isSuccessful(), Toast.LENGTH_SHORT).show();
+                        if(!task.isSuccessful()){
+                            Toast.makeText(Signup.this,"Authentication Falied" +task.getException(), Toast.LENGTH_SHORT).show();
+                        }else{
+                            startActivity(new Intent(Signup.this, Home.class));
+                            finish();
+                        }
+                    }
+                });//addOnCompleteListener(SignupActivity.this, new OnCompleteListener<AuthResult>()
             }
-        }
+        });
+    }
+
 
     public void btn_back(View view) {
         Intent intent1 = new Intent(Signup.this, MainActivity.class);
         startActivity(intent1);
     }
+
 }
