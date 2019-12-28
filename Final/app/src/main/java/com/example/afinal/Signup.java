e73f46e23db71c3f78b7bba2fe9bec88600fdfeb
Daliasalah <saliasary46@gmail.com>
2019-12-28 11:40:15 +0200

    edit authentication

--- Final/app/src/main/java/com/example/afinal/Signup.java
+++ Final/app/src/main/java/com/example/afinal/Signup.java
@@ -1,5 +1,6 @@
 package com.example.afinal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,7 +8,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Signup extends AppCompatActivity {
@@ -27,22 +32,33 @@ public class Signup extends AppCompatActivity {
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-             String email1 = emailId.getText().toString();
-             String psw = password.getText().toString();
-             if(email1.isEmpty()){
-                 emailId.setError("please enter email id");
-                 emailId.requestFocus();
-             }
-             else if(psw.isEmpty()){
-                 password.setError("please enter password");
-                 password.requestFocus();
-             }
+                String email1 = emailId.getText().toString();
+                String psw = password.getText().toString();
+                if (email1.isEmpty()) {
+                    emailId.setError("please enter email ");
+                    emailId.requestFocus();
+                } else if (psw.isEmpty()) {
+                    password.setError("please enter password");
+                    password.requestFocus();
+                } else if (!(email1.isEmpty() && psw.isEmpty())) {
+                    mFirebaseAuth.createUserWithEmailAndPassword(email1, psw).addOnCompleteListener
+                            (Signup.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (!task.isSuccessful()) {
+                                        Toast.makeText(Signup.this, "Created account is not succeful, try again", Toast.LENGTH_SHORT).show();
+                                    } else {
+                                        startActivity(new Intent(Signup.this, Home.class));
+                                    }
+                                }
+                            });
+                } else {
+                    Toast.makeText(Signup.this, "Created account is not succeful, try again", Toast.LENGTH_SHORT).show();
+                }
+
             }
-        }){
-
-        }
-    }
-
+        });
+}
 
     public void btn_back(View view) {
         Intent intent1 = new Intent(Signup.this, MainActivity.class);
